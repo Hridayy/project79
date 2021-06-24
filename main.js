d07c28b66ebf431256c98790786e1cf688ab778c
Hridayy <73392185+Hridayy@users.noreply.github.com>
2021-06-24 16:26:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+menu_list_array=["chicken Tandoori Pizza",
+                  "Veg Supreme Pizza",
+                  "Paneer Tikka Pizza",
+                  "Deluxe Veggie Pizza",
+                  "Veg Extravaganza Pizza"];                   
